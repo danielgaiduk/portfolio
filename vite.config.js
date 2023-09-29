0b3fe4e7991d3f307d8bf56e54cb9b49233ce31d
Daniel Gaiduk <daniel@gaiduk.dev>
2023-09-29 15:52:15 +0200

    Remove https certificates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import { sentrySvelteKit } from '@sentry/sveltekit'
 import { sveltekit } from '@sveltejs/kit/vite'
-import fs from 'fs'
 
 /** @type {import('vitest/config').UserConfigExport} */
 export default defineConfig({
@@ -16,12 +15,5 @@ export default defineConfig({
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	server: {
-		https: {
-			key: fs.readFileSync(`${__dirname}/../certificates/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/../certificates/cert.pem`)
-		},
-		proxy: {}
 	}
 })
