4ddd7183f60ce0eff4724242e4138810918da73e
Daniel Gaiduk <daniel@gaiduk.dev>
2023-10-02 18:16:22 +0200

    Replace logo with svg

--- src/lib/components/elements/Navbar.svelte
+++ src/lib/components/elements/Navbar.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { EMAIL, PHONE_NUMBER } from '$lib/constants'
-	import { HamburgerIcon, Link, Delimiter, SideNavigation } from '$lib/components'
+	import { HamburgerIcon, Link, Delimiter, SideNavigation, LogoIcon } from '$lib/components'
 	import { t } from '$lib/translations'
 	import { toValidPhoneNumber } from '$lib/utils'
-	import logo from '$lib/images/logo-white.png'
 
 	const LINK_MAPPING = [
 		{ href: `tel:${toValidPhoneNumber(PHONE_NUMBER)}`, label: PHONE_NUMBER },
@@ -17,13 +16,7 @@
 	class="sticky top-0 z-40 bg-primary-darker flex items-center justify-between p-4 border-b border-primary-normal/50"
 >
 	<Link class="no-animation" href="/" label={$t('navbar.logo_link_label')}>
-		<img
-			width="100%"
-			height="100%"
-			class="w-16 sm:w-20 lg:w-24 h-auto"
-			src={logo}
-			alt={$t('navbar.logo')}
-		/>
+		<LogoIcon class="w-16 sm:w-20 lg:w-24" />
 	</Link>
 	<div class="hidden md:block w-auto">
 		{#each LINK_MAPPING as { href, label }, i}
