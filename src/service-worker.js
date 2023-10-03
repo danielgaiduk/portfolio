f5f3cae6ffd577e63e16568999375dc95ac315af
Daniel Gaiduk <daniel@gaiduk.dev>
2023-10-03 02:37:40 +0200

    Rewrite service-worker to js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,15 +1,11 @@
 /// <reference types="@sveltejs/kit" />
-/// <reference no-default-lib="true"/>
-/// <reference lib="esnext" />
-/// <reference lib="webworker" />
-
 import { build, files, version } from '$service-worker'
 
 const CACHE = `cache-${version}`
-const ASSETS = [...build, ...files]
-const SW = self as unknown as ServiceWorkerGlobalScope
 
-SW.addEventListener('install', (event) => {
+const ASSETS = [...build, ...files]
+
+self.addEventListener('install', (event) => {
 	async function addFilesToCache() {
 		const cache = await caches.open(CACHE)
 		await cache.addAll(ASSETS)
@@ -18,7 +14,7 @@ SW.addEventListener('install', (event) => {
 	event.waitUntil(addFilesToCache())
 })
 
-SW.addEventListener('activate', (event) => {
+self.addEventListener('activate', (event) => {
 	async function deleteOldCaches() {
 		for (const key of await caches.keys()) {
 			if (key !== CACHE) await caches.delete(key)
@@ -28,27 +24,27 @@ SW.addEventListener('activate', (event) => {
 	event.waitUntil(deleteOldCaches())
 })
 
-SW.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
 	if (event.request.method !== 'GET') return
 
-	async function respond(): Promise<Response> {
+	async function respond() {
 		const url = new URL(event.request.url)
 		const cache = await caches.open(CACHE)
 
 		if (ASSETS.includes(url.pathname)) {
-			return (await cache.match(url.pathname)) as Response
+			return cache.match(url.pathname)
 		}
 
 		try {
 			const response = await fetch(event.request)
 
 			if (response.status === 200) {
-				await cache.put(event.request, response.clone())
+				cache.put(event.request, response.clone())
 			}
 
 			return response
 		} catch {
-			return (await cache.match(event.request)) as Response
+			return cache.match(event.request)
 		}
 	}
 
