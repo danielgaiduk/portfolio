eaf38a4275a6519259c9fdf5685462cb3960bee8
Daniel Gaiduk <daniel@gaiduk.dev>
2023-10-03 02:38:30 +0200

    Rewrite background image to static folder

--- src/lib/components/sections/Start.svelte
+++ src/lib/components/sections/Start.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	import { Headline, Text, Wrapper, FlyUp, Picture } from '$lib/components'
+	import { Headline, Text, Wrapper, FlyUp } from '$lib/components'
 	import { t } from '$lib/translations'
-	// @ts-ignore
-	import background from '$lib/images/background.webp?w=500;900;1200&format=avif;webp;jpg&as=picture'
 </script>
 
 <Wrapper id="start">
@@ -15,9 +13,10 @@
 		</Headline>
 	</div>
 	<Text class="max-w-2xl mx-auto text-center">
+		<!-- eslint-disable-next-line -->
 		{@html $t('start.quote')}
 	</Text>
 	<FlyUp class="flex items-center justify-center">
-		<Picture loading="eager" meta={background} alt={$t('start.image')} />
+		<img src="/images/background.webp" alt={$t('start.image')} />
 	</FlyUp>
 </Wrapper>
