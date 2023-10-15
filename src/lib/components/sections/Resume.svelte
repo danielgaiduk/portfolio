7071a5580a3bc1206de60f1dabad66107e496eb4
Daniel Gaiduk <daniel@gaiduk.dev>
2023-10-15 23:45:04 +0200

    Change resume technologies

--- src/lib/components/sections/Resume.svelte
+++ src/lib/components/sections/Resume.svelte
@@ -8,87 +8,130 @@
 			company: 'View GmbH',
 			date: '01/2023 - 09/2023',
 			description: $t('resume.view.description'),
-			technologies: ['Svelte', 'SvelteKit', 'TypeScript', 'Pocketbase', 'TailwindCss']
+			technologies: [
+				'HTML5',
+				'CSS3',
+				'TypeScript',
+				'Svelte',
+				'SvelteKit',
+				'PostCSS',
+				'TailwindCSS',
+				'Pocketbase',
+				'Prisma',
+				'Vitest',
+				'Playwright',
+				'Storybook'
+			]
 		},
 		{
 			title: $t('resume.micromerce.title'),
 			company: 'Micromerce GmbH',
 			date: '10/2022 - 06/2023',
 			description: $t('resume.micromerce.description'),
-			technologies: ['TypeScript', 'NodeJs', 'XML', 'CSV']
+			technologies: ['TypeScript', 'Node.js', 'Jest']
 		},
 		{
 			title: $t('resume.pwc.title'),
 			company: 'PwC GmbH',
 			date: '08/2022 - 10/2022',
 			description: $t('resume.pwc.description'),
-			technologies: ['React', 'Next', 'GraphQL', 'Apollo Client', 'MaterialUI']
+			technologies: [
+				'HTML5',
+				'CSS3',
+				'TypeScript',
+				'React',
+				'Next',
+				'Redux',
+				'Material-UI',
+				'GraphQL',
+				'Cypress'
+			]
 		},
 		{
 			title: $t('resume.telekom.title'),
 			company: 'Telekom AG',
 			date: '07/2022 - 08/2022',
 			description: $t('resume.telekom.description'),
-			technologies: ['React', 'Next', 'GraphQL', 'Apollo Client', 'MaterialUI']
+			technologies: [
+				'HTML5',
+				'CSS3',
+				'TypeScript',
+				'React',
+				'Next',
+				'Redux',
+				'Material-UI',
+				'GraphQL',
+				'Cypress'
+			]
 		},
 		{
 			title: $t('resume.plentymarkets.title'),
 			company: 'Plentysystems AG',
 			date: '10/2021 - 08/2022',
 			description: $t('resume.plentymarkets.description'),
-			technologies: ['PHP', 'Vue', 'Twig']
+			technologies: ['HTML5', 'CSS3', 'JavaScript', 'PHP', 'Twig']
 		},
 		{
 			title: $t('resume.swym.title'),
 			company: 'Swym',
 			date: '06/2021 - 08/2021',
 			description: $t('resume.swym.description'),
-			technologies: ['Vue', 'Nuxt', 'TypeScript', 'Scss', 'Material Design']
+			technologies: [
+				'HTML5',
+				'CSS3',
+				'TypeScript',
+				'React',
+				'Next',
+				'Redux',
+				'Ant Design',
+				'GraphQL',
+				'Cypress'
+			]
 		},
 		{
 			title: $t('resume.gaiduk.title'),
 			company: 'Gühnter-Gaiduk Immobilien',
 			date: '08/2021 - 12/2021',
 			description: $t('resume.gaiduk.description'),
-			technologies: [
-				'React',
-				'Next',
-				'TypeScript',
-				'Jest',
-				'Cypress',
-				'Redux',
-				'GraphQL',
-				'Apollo Client',
-				'TailwindCss'
-			]
+			technologies: ['HTML5', 'CSS3', 'TypeScript', 'React', 'Next', 'TailwindCSS', 'GraphQL']
 		},
 		{
 			title: $t('resume.quirin.title'),
 			company: 'Quirin Privatbank AG',
 			date: '12/2020 - 06/2021',
 			description: $t('resume.quirin.description'),
-			technologies: ['Svelte', 'Routify', 'TypeScript', 'TailwindCss', 'Jest', 'Scss']
+			technologies: ['HTML5', 'CSS3', 'TypeScript', 'Svelte', 'Routify', 'PostCSS', 'TailwindCSS']
 		},
 		{
 			title: $t('resume.eam.title'),
 			company: 'EAM GmbH & Co. KG',
 			date: '01/2020 - 01/2021',
 			description: $t('resume.eam.description'),
-			technologies: ['Svelte', 'Sapper', 'TypeScript', 'TailwindCss', 'Jest', 'Scss']
+			technologies: ['HTML5', 'CSS3', 'TypeScript', 'Svelte', 'Sapper', 'PostCSS', 'TailwindCSS']
 		},
 		{
 			title: $t('resume.insignio.title'),
 			company: 'Insignio CRM GmbH',
 			date: '08/2018 - 06/2021',
 			description: $t('resume.insignio.description'),
-			technologies: []
+			technologies: [
+				'HTML5',
+				'CSS3',
+				'JavaScript',
+				'PHP',
+				'Sugar CRM',
+				'Wordpress',
+				'Typo3',
+				'Svelte',
+				'Routify',
+				'Sapper'
+			]
 		}
 	]
 </script>
 
 <Wrapper id="resume">
 	<Headline type="h2">{$t('resume.headline')}</Headline>
-
 	{#each WORK_EXPERIENCE_MAPPING as { title, company, date, description, technologies }, i}
 		<div>
 			<Text class="font-bold uppercase text-2xl text-white">{title}</Text>
@@ -101,7 +144,7 @@
 				<div class="flex flex-wrap gap-2 mt-4">
 					{#each technologies as technologie}
 						<Text
-							class="font-semibold text-white lowercase text-base"
+							class="font-semibold text-white text-base"
 							wrapperClass="h-10 w-40 border border-primary-normal/50 rounded-l-lg rounded-r-lg flex items-center justify-center"
 						>
 							{technologie}
